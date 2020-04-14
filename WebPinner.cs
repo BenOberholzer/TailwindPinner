b00ac39e30a7eb41585e310bbef025b5fe31b505
Unknown <benneto@gmail.com>
2020-04-14 16:48:11 -0400

    Moving creds to command line arguments

--- WebPinner.cs
+++ WebPinner.cs
@@ -20,7 +20,7 @@ namespace Pinner
             IWebDriver driver = new ChromeDriver(options);
             //driver.Manage().Window.Maximize();
 
-            WebPinner.TailwindLogin(driver);
+            WebPinner.TailwindLogin(driver, args);
             
             WebPinner.getS6Products(driver);
 
@@ -29,7 +29,7 @@ namespace Pinner
             //driver.Quit();
         }
 
-        static private IWebDriver TailwindLogin(IWebDriver driver) {
+        static private IWebDriver TailwindLogin(IWebDriver driver, string[] args) {
             string home = "https://tailwindapp.com/login";
             driver.Navigate().GoToUrl(home);
 
@@ -37,8 +37,8 @@ namespace Pinner
             IWebElement password = driver.FindElement(By.Name("password"));
             IWebElement login = driver.FindElement(By.ClassName("btn-login"));
 
-            username.SendKeys("oberholzer.sam@gmail.com");
-            password.SendKeys("31ndyh177");
+            username.SendKeys(args[0]);
+            password.SendKeys(args[1]);
 
             login.Click();
             
@@ -54,10 +54,16 @@ namespace Pinner
 
             try {
                 links = driver.FindElements(By.ClassName("title_product_1YKi0"));
-            } catch(NoSuchElementException e) {
+            }
+            catch (NoSuchElementException e) {
                 IWebElement advert = driver.FindElement(By.ClassName("bx-close-link"));
                 advert.Click();
-            } finally {
+            } 
+            catch (StaleElementReferenceException e) {
+                IWebElement advert = driver.FindElement(By.ClassName("bx-close-link"));
+                advert.Click();
+            }
+            finally {
                 links = driver.FindElements(By.ClassName("title_product_1YKi0"));
             }
             
@@ -75,6 +81,10 @@ namespace Pinner
                     IWebElement advert = driver.FindElement(By.ClassName("bx-close-link"));
                     advert.Click();
                 }
+                catch (StaleElementReferenceException e) {
+                    IWebElement advert = driver.FindElement(By.ClassName("bx-close-link"));
+                    advert.Click();
+                }
                 finally {
                     url = item.GetAttribute("href");
                 }
@@ -89,17 +99,12 @@ namespace Pinner
                 driver.Navigate().GoToUrl(prodLink);
                 Thread.Sleep(2000);
 
-                //
-
-                // get all the buttons and then use the first one
                 WebPinner.ClickScheduleButton(driver);
-                //clicker.MoveToElement(scheduleButton).Click().Perform();
                 
                 Thread.Sleep(2500);
 
                 Console.WriteLine("going home");
                 driver.Navigate().GoToUrl(home);
-                //WebPinner.OpenNewTab(item.GetAttribute("href"), driver);
                 Thread.Sleep(2500);
 
                 if (++count > 5) {
@@ -111,17 +116,20 @@ namespace Pinner
         }
 
         public static void ClickScheduleButton(IWebDriver driver) {
+            // get all the buttons and then use the first one
             IList<IWebElement> buttons = driver.FindElements(By.XPath("//*[@id='tw_schedule_btn']"));
+            Console.WriteLine("Number of items found: " + buttons.Count());
             IWebElement scheduleButton = buttons.ElementAt(0);
 
-            TimeSpan tspan = new TimeSpan(100);
+            /*TimeSpan tspan = new TimeSpan(50000000);
             WebDriverWait wait = new WebDriverWait(driver, tspan);
 //            wait.Until(ExpectedConditions.visibilityOfElementLocated(By.XPath("//*[@id='tw_schedule_btn']")));
-            wait.Until(WebPinner.ElementIsVisibleAndEnabled(scheduleButton));
+            wait.Until(WebPinner.ElementIsVisibleAndEnabled(scheduleButton));*/
             
             Actions actions = new Actions(driver);
-            actions.MoveToElement(scheduleButton).Perform();
-            actions.MoveToElement(scheduleButton).Click().Perform();
+            //actions.MoveToElement(scheduleButton).Perform();
+            actions.MoveToElement(scheduleButton).Click();
+            actions.Perform();
         }
 
         public static Func<IWebDriver, bool> ElementIsVisibleAndEnabled(IWebElement element) {
